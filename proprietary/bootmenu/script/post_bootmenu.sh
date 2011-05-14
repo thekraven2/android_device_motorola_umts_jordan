ced4485a655198993325a13e3c600621e0dc9ed3
Quarx2k <agent00791@gmail.com>
2011-05-14 19:35:25 +0700

    update bootmenu 0.8

--- proprietary/bootmenu/script/post_bootmenu.sh
+++ proprietary/bootmenu/script/post_bootmenu.sh
@@ -1,28 +1,27 @@
 #!/sbin/sh
 
-######## BootMenu Script v0.6.1
+######## BootMenu Script v0.8.0
 ######## Execute Post BootMenu
 
 
-export PATH=/sbin:/system/xbin:/system/bin:system/bootmenu/binary
+export PATH=/sbin:/system/xbin:/system/bin
 
 ######## Main Script
 
-#### kill adbd
 
-######## Fix me..
-killall -9 adbd
-killall -9 adbd.root
+## Run Init Script
 
-##kill -9 $(/sbin/ps |
-##grep 'adbd' |
-##grep -v 'grep' |
-##cut -d ' ' -f2)
-########
+######## Don't Delete.... ########################
+mount -o remount,rw rootfs /
+mount -o remount,rw /dev/block/mmcblk1p21 /system
+##################################################
 
+chmod 755 /system/bootmenu/init.d/*
+run-parts /system/bootmenu/init.d/
 
-######## Don't Delete.... ############
+######## Don't Delete.... ########################
 mount -o remount,ro rootfs /
-######################################
+mount -o remount,ro /dev/block/mmcblk1p21 /system
+##################################################
 
 exit
--- proprietary/bootmenu/script/pre_bootmenu.sh
+++ proprietary/bootmenu/script/pre_bootmenu.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 
-######## BootMenu Script v0.6.1
+######## BootMenu Script v0.8.0
 ######## Execute Pre BootMenu
 
 
-export PATH=/sbin:/system/xbin:/system/bin:system/bootmenu/binary
+export PATH=/sbin:/system/xbin:/system/bin
 
 ######## Main Script
 
@@ -19,21 +19,12 @@ $BUSYBOX chmod 755 /sbin/
 $BUSYBOX chmod 755 /sbin/busybox
 $BUSYBOX chown 0.0 /sbin/busybox
 
+
 ## begin busybox sym link..
 
 for cmd in $(/sbin/busybox --list); do
   /sbin/busybox ln -s /sbin/busybox /sbin/$cmd
 done
-
-## sshd dropbear permissions
-
-$BUSYBOX cp -f /system/bootmenu/dropbear/* /sbin/
-$BUSYBOX chmod 755 /sbin/dropbear
-$BUSYBOX chmod 755 /sbin/sftp-server
-$BUSYBOX chmod 755 /sbin/ssh
-$BUSYBOX chmod 755 /sbin/scp
-$BUSYBOX chown 0.0 /sbin/dropbear
-$BUSYBOX chown 0.0 /sbin/sftp-server
 
 ## rootsh create.
 
@@ -52,10 +43,5 @@ cp -f /system/bootmenu/config/default.prop /default.prop
 
 cp -f /system/bootmenu/binary/adbd /sbin/adbd.root
 chmod 755 /sbin/adbd.root
-
-## Run Init Script
-
-chmod 755 /system/bootmenu/init.d/*
-run-parts /system/bootmenu/init.d/
 
 exit
