3fa25824e41f798af6d957899519e4ed6d952c74
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-05-14 16:20:26 +0200

    use 2nd-boot for dropbear (test purpose)

---
+++ proprietary/bootmenu/script/2nd-boot.sh
@@ -0,0 +1,14 @@
+#!/sbin/ash
+
+######## BootMenu Script
+######## Execute [2nd-boot] Menu
+# Used to test Dropbear
+
+export PATH=/sbin:/system/xbin:/system/bin
+
+######## Main Script
+
+. /system/bootmenu/script/dropbear.sh
+
+
+exit
