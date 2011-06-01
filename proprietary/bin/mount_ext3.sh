2c5f9a0f5fd9c2807989d7abe11313d647cde0aa
Quarx2k <agent00791@gmail.com>
2011-06-02 00:24:11 +0700

    barrier=0

--- proprietary/bin/mount_ext3.sh
+++ proprietary/bin/mount_ext3.sh
@@ -3,4 +3,4 @@ export PATH=/sbin:/system/xbin:/system/bin
 PART_ALIAS=$1
 MOUNT_POINT=$2
 BLOCK_DEVICE=/dev/block/${PART_ALIAS}
-mount -t ext3 -o nosuid,nodev,noatime,nodiratime,barrier=1 ${BLOCK_DEVICE} ${MOUNT_POINT}
+mount -t ext3 -o nosuid,nodev,noatime,nodiratime,barrier=0 ${BLOCK_DEVICE} ${MOUNT_POINT}
